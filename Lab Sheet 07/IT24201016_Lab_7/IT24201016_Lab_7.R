37a65942fd763d93cb22f9f729960d14056c5e78
IT24201016 <IT24201016@my.sliit.lk>
2025-09-16 12:17:07 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24201016_Lab_7/IT24201016_Lab_7.R
@@ -0,0 +1,12 @@
+setwd ("C:\\Users\\nayan\\OneDrive\\Desktop\\IT24201016_Lab_7")
+
+
+punif(25,min=0,max=40)-punif(10,min=0,max=40)
+
+pexp(2,rate=1)
+
+pexp(2,rate =1/3)
+
+pnorm(130,mean=100, sd=15, lower.tail =FALSE)
+
+qnorm(0.95, mean=100,sd=15)
